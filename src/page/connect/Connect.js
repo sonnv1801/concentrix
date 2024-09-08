413d019184875c4c8cd78f5edae500ece6fe0e83
sonnv1801 <nguyenvanson18102001@gmail.com>
2024-09-08 22:40:36 +0700

    add0809

---
+++ src/page/connect/Connect.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+import "./style.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const Connect = () => {
+  const [isOnline, setIsOnline] = useState(navigator.onLine);
+
+  useEffect(() => {
+    const handleOnline = () => {
+      setIsOnline(true);
+      toast("Quay lại trực tuyến", { type: "success" });
+    };
+
+    const handleOffline = () => {
+      setIsOnline(false);
+      toast("Không có mạng. Kiểm tra lại kết nối!", { type: "error" });
+    };
+
+    window.addEventListener("online", handleOnline);
+    window.addEventListener("offline", handleOffline);
+
+    return () => {
+      window.removeEventListener("online", handleOnline);
+      window.removeEventListener("offline", handleOffline);
+    };
+  }, []);
+
+  return (
+    <>
+      <ToastContainer />
+    </>
+  );
+};
+
+export default Connect;
